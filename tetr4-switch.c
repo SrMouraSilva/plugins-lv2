7dc156c9176f646abe1df6bd8755e34453e290a4
Paulo Mateus <mateus.moura@sti.ufc.br>
2021-12-01 14:36:29 -0300

    Initial GUI

--- tetr4-switch.c
+++ tetr4-switch.c
@@ -3,12 +3,12 @@
 
 #include "lv2/lv2plug.in/ns/lv2core/lv2.h"
 
-#define PLUGIN_URI "http://moddevices.com/plugins/mod-devel/mod-cv-control"
+#define PLUGIN_URI "http://srmourasilva.github.io/plugins/tetr4-switch"
 
 typedef enum {
-	L_OUTPUT    = 0,
-	L_LEVEL     = 1,
-	L_SMOOTHING = 2
+    L_OUTPUT    = 0,
+    L_LEVEL     = 1,
+    L_SMOOTHING = 2
 } PortIndex;
 
 typedef struct {
@@ -33,48 +33,37 @@ instantiate(const LV2_Descriptor*     descriptor,
     self->b1 = exp(-2.0 * M_PI * frequency);
     self->a0 = 1.0 - self->b1;
 
-	return (LV2_Handle)self;
+    return (LV2_Handle)self;
 }
 
-static void
-connect_port(LV2_Handle instance,
-             uint32_t   port,
-             void*      data)
-{
-	Control* self = (Control*)instance;
+static void connect_port(LV2_Handle instance, uint32_t port, void* data) {
+    Control* self = (Control*) instance;
 
-	switch ((PortIndex)port) {
-	case L_OUTPUT:
-		self->output = (float*)data;
-		break;
-	case L_LEVEL:
-		self->level = (float*)data;
-        break;
-	case L_SMOOTHING:
-		self->smoothing = (float*)data;
-        break;
-	}
+    switch ((PortIndex)port) {
+        case L_OUTPUT:
+            self->output = (float*)data;
+            break;
+        case L_LEVEL:
+            self->level = (float*)data;
+            break;
+        case L_SMOOTHING:
+            self->smoothing = (float*)data;
+            break;
+    }
 }
 
-static void
-activate(LV2_Handle instance)
-{
+static void activate(LV2_Handle instance) {
 }
 
-static double
-lowPassProcess(Control* self, float input)
-{
+static double lowPassProcess(Control* self, float input) {
     return self->z1 = input * self->a0 + self->z1 * self->b1;
 }
 
-static void
-run(LV2_Handle instance, uint32_t n_samples)
-{
+static void run(LV2_Handle instance, uint32_t n_samples) {
     Control* self = (Control*) instance;
     float coef = *self->level;
 
-    for ( uint32_t i = 0; i < n_samples; i++)
-    {
+    for (uint32_t i = 0; i < n_samples; i++) {
         float smooth = lowPassProcess(self, coef);
 
         if ((int)*self->smoothing == 1) {
@@ -85,40 +74,31 @@ run(LV2_Handle instance, uint32_t n_samples)
     }
 }
 
-static void
-deactivate(LV2_Handle instance)
-{
+static void deactivate(LV2_Handle instance) {
 }
 
-static void
-cleanup(LV2_Handle instance)
-{
-	free(instance);
+static void cleanup(LV2_Handle instance) {
+    free(instance);
 }
 
-static const void*
-extension_data(const char* uri)
-{
-	return NULL;
+static const void* extension_data(const char* uri) {
+    return NULL;
 }
 
 static const LV2_Descriptor descriptor = {
-	PLUGIN_URI,
-	instantiate,
-	connect_port,
-	activate,
-	run,
-	deactivate,
-	cleanup,
-	extension_data
+    PLUGIN_URI,
+    instantiate,
+    connect_port,
+    activate,
+    run,
+    deactivate,
+    cleanup,
+    extension_data
 };
 
-LV2_SYMBOL_EXPORT
-const LV2_Descriptor*
-lv2_descriptor(uint32_t index)
-{
-	switch (index) {
-	case 0:  return &descriptor;
-	default: return NULL;
-	}
+LV2_SYMBOL_EXPORT const LV2_Descriptor* lv2_descriptor(uint32_t index) {
+    switch (index) {
+        case 0:  return &descriptor;
+        default: return NULL;
+    }
 }
